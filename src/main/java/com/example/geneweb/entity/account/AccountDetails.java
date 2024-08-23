e7567a7826ec99b37f7f4d5bb8fe1575285145b7
jisoo0609 <jisu20000609@gmail.com>
2024-08-23 23:02:29 +0900

    Update: add AccountDetails

---
+++ src/main/java/com/example/geneweb/entity/account/AccountDetails.java
@@ -0,0 +1,67 @@
+package com.example.geneweb.entity.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDetails implements UserDetails {
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private boolean mailAuth;
+    private Authority authority;
+    private Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (authority != null) {
+            String role = authority.getAuthority();
+            return Collections.singleton(new SimpleGrantedAuthority(role));
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
